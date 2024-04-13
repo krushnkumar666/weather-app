988f578a28f6a76d797aacbfc626628f3e7da1b6
Krushna Bawalge <krushnkumar403@gmail.com>
2024-04-14 00:53:54 +0530

    first commit

---
+++ src/api.js
@@ -0,0 +1,13 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '593bafa1c5mshd8a34ac71e94daap1233dajsn2b6371a1bc3c',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+
+export const WEATHER_API_KEY = "b7a55a0ba877384cd34ee86f0a2151ba";
